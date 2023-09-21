52430e2caee2b0aa4005625590d2b22c42451d7e
corsi.20188 <corsi.20188@E2-9-PC26>
2023-09-21 10:31:10 +0200

    Aggiungere i file di progetto.

---
+++ 2 - Rappresentare una persona fisica/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___Rappresentare_una_persona_fisica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Classe2/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Classe2
+{
+    public class Class1
+    {
+    }
+
+    public class Persona
+    {
+        // attributi
+        private int codice;
+        private string cognome;
+        private string nome;
+        private bool registrato;
+
+        public string Output()
+        {
+            if(registrato == true)
+            {
+                string stringa = codice + cognome + nome + registrato;
+                return stringa;
+            }else
+                return string.Empty;
+        }
+
+        // costruttore 
+        public Persona(int codice, string cognome, string nome, bool registrato) 
+        {
+            setCodice(codice);
+            setCognome(cognome);
+            setNome(nome);
+            setRegistrato(registrato);
+        }
+
+        public void setCodice(int codice)
+        {
+            this.codice = codice;
+        }
+        public void setCognome(string cognome)
+        {
+            this.cognome = cognome;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public void setRegistrato(bool registrato)
+        {
+            this.registrato = registrato;
+        }
+    }
+}
